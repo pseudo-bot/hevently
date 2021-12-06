839323a3157a78707a32d6fef94830ff9e644727
saranggupta11 <sarang.gupta0811@gmail.com>
2021-12-06 14:50:33 +0530

    footer initial commit

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,17 @@
-import Navbar from '../components/Navbar/Navbar';
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Navbar/Footer";
 
-import 'tailwindcss/tailwind.css';
-import '../style/global.css'
+import "tailwindcss/tailwind.css";
+import "../style/global.css";
 
 function App({ Component, pageProps }) {
-	return (
-		<>
-			<Navbar />
-			<Component {...pageProps} />
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
