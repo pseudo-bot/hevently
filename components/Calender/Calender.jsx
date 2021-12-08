af1b358f3becc85f981620e1a05499bdb0033121
saranggupta11 <sarang.gupta0811@gmail.com>
2021-12-08 13:52:03 +0530

    calender added

---
+++ components/Calender/Calender.jsx
@@ -0,0 +1,30 @@
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
+import { useState } from "react";
+const Calender = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+    console.log(ranges.selection.startDate);
+    console.log(ranges.selection.endDate);
+  };
+  return (
+    <div className="flex justify-center py-8">
+      <DateRangePicker
+        ranges={[selectionRange]}
+        minDate={new Date()}
+        onChange={handleSelect}
+      />
+    </div>
+  );
+};
+
+export default Calender;
--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -6,16 +6,21 @@ const Contact = () => {
       <div className="dancing text-5xl capitalize md:text-7xl text-gray-700 font-bold text-center mt-24 mb-8">
         Contact Us
       </div>
+<<<<<<< Updated upstream
       <div className="container mx-auto flex sm:flex-nowrap flex-wrap gap-10 px-5">
+=======
+      <div className="container px-5 mx-auto flex sm:flex-nowrap flex-wrap">
+>>>>>>> Stashed changes
         <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 hidden sm:flex items-end justify-start relative">
           <iframe
             width="100%"
             height="100%"
             className="absolute inset-0"
+<<<<<<< Updated upstream
             frameborder="0"
+=======
+>>>>>>> Stashed changes
             title="map"
-            marginheight="0"
-            marginwidth="0"
             scrolling="no"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d234447.66995607846!2d85.18124037459347!3d23.3431424976028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4e104aa5db7dd%3A0xdc09d49d6899f43e!2sRanchi%2C%20Jharkhand!5e0!3m2!1sen!2sin!4v1638902154806!5m2!1sen!2sin"
           ></iframe>
@@ -33,7 +38,13 @@ const Contact = () => {
               <h2 className="title-font font-bold text-gray-900 tracking-widest text-xs">
                 EMAIL
               </h2>
+<<<<<<< Updated upstream
               <a className="text-indigo-500 leading-relaxed">example@email.com</a>
+=======
+              <a className="text-indigo-500 leading-relaxed">
+                example@email.com
+              </a>
+>>>>>>> Stashed changes
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
                 PHONE
               </h2>
