c4454fc68470c8f0328d03654b91404f90467276
Shivam <shivam66637@gmail.com>
2021-12-09 01:07:37 +0530

    navbar fix

--- components/Navbar/NavLinks.jsx
+++ components/Navbar/NavLinks.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
-const NavItem = ({ children }) => {
+const NavItem = ({ children, services=0 }) => {
 	return (
-		<div className="p-3 lg:p-0 lg:py-2 border lg:border-none w-full text-gray-500 uppercase tracking-wider lg:text-gray-200 lg:text-center transition-all duration-150 hover:text-[blue] lg:rounded-full">
+		<div className={`p-3 lg:p-0 lg:py-2 border lg:border-none w-full text-gray-500 uppercase tracking-wider lg:text-gray-200 lg:text-center transition-all duration-150 hover:text-[blue] ${services ? 'h-[3rem] flex items-center gap-4' : ''}`}>
 			{children}
 		</div>
 	);
@@ -25,18 +25,18 @@ export default function NavLinks({ hidden }) {
 			<NavItem>About</NavItem>
 			<div onClick={() => setServices(!services)} className="w-full relative services-div">
 				<NavItem>Services</NavItem>
-				<div className="absolute top-3 right-4 lg:hidden">
+				<div className="absolute top-3 right-4 lg:hidden ">
 					{services ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
 				</div>
 				<div
 					className={`${
 						services ? 'h-[12rem]' : 'h-0'
-					} lg:fixed lg:top-16 lg:w-56 overflow-hidden transition-all duration-500 w-full bg-gray-100 font-normal services `}
+					} lg:fixed lg:top-16 lg:w-56 lg:pl-4 overflow-hidden transition-all duration-500 w-full bg-gray-100 font-normal services text-sm`}
 				>
-					<NavItem><img src="/icons/wedding.png"></img>Weddings</NavItem>
-					<NavItem><img src="/icons/gathering.png"></img>Social Gathering</NavItem>
-					<NavItem><img src="/icons/cake.png"></img>Birthdays</NavItem>
-					<NavItem><img src="/icons/corporate.png"></img>Corporate Events</NavItem>
+					<NavItem services={1}><img src="/icons/wedding.png"></img>Weddings</NavItem>
+					<NavItem services={1}><img src="/icons/gathering.png"></img>Social Gathering</NavItem>
+					<NavItem services={1}><img src="/icons/cake.png"></img>Birthdays</NavItem>
+					<NavItem services={1}><img src="/icons/corporate.png"></img>Corporate Events</NavItem>
 				</div>
 			</div>
 
