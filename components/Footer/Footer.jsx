f3c5ff6cec674524e506776924a2131e31b5b7e6
pseudobot <shivam66637@gmail.com>
2021-12-08 11:42:31 +0530

    footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -6,7 +6,6 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 const Footer = () => {
   return (
-
     <footer className="text-gray-50 bg-wgray-100 mt-6">
       <div className="container px-12 pt-12 pb-8 md:pb-0 mx-auto">
         <div className="sm:flex flex-wrap justify-evenly order-first items-center">
@@ -26,7 +25,6 @@ const Footer = () => {
                   <InstagramIcon />
                 </a>
                 <a className="ml-3 text-gray-500 cursor-pointer hover:text-gray-800 mr-4">
-
                   <LinkedInIcon />
                 </a>
               </span>
@@ -112,10 +110,7 @@ const Footer = () => {
                 <input
                   type="text"
                   placeholder="Email..."
-
-
                   className="w-full bg-cgray-50 bg-opacity-50 rounded border-gray-400 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-blue-500 outline-none text-gray-600 py-1 px-2 leading-8 transition-colors duration-200 ease-in-out text-base border-2"
-
                 />
               </div>
               <button className="flex-shrink-0 inline-flex bg-blue-500 border-0 py-2 px-4 focus:outline-none hover:bg-blue-600 rounded mt-2">
@@ -129,7 +124,6 @@ const Footer = () => {
       <div className="bg-blue-600 mt-2">
         <div className="container py-4 mx-auto flex items-center justify-center">
           <p className="text-sm font-semibold text-gray-50">
-
             Copyright © 2021 TheLogicTech
           </p>
         </div>
