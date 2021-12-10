fb99a2ae020d278b757198f503a579bf09ac5a8f
Shivam <shivam66637@gmail.com>
2021-12-10 18:05:39 +0530

    add footer about

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,9 +1,11 @@
 import About from '../components/About/About'
+import Footer from '../components/Footer/Footer';
 
 function AboutPage() {
 	return (
 		<div>
 			<About />
+			<Footer />
 		</div>
 	);
 }
