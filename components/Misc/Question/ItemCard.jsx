3501cb2ee1e226de023813d29b29b85fd869cc13
Shivam <shivam66637@gmail.com>
2021-12-11 21:12:31 +0530

    structure change

--- components/Misc/Question/ItemCard.jsx
+++ components/Misc/Question/ItemCard.jsx
@@ -1,40 +1,20 @@
 import StarIcon from '@mui/icons-material/Star';
-import { Divider } from '@mui/material';
+import { Divider, Rating } from '@mui/material';
 
-const Star = () => {
-	return (
-		<StarIcon
-			sx={{
-				fontSize: '2ch',
-				color: '#eba800',
-			}}
-		/>
-	);
-};
-
-const ItemCard = ({ venue, city, ratings, capacity }) => {
+const ItemCard = ({ value, city, ratings, capacity }) => {
 	return (
 		<div data-aos="fade-up">
 			<div className="flex flex-col w-full gap-2 p-4 border-blue-400 rounded-md shadow-md bg-cgray-50 cursor-pointer hover:scale-105 transition-all duration-200">
 				<div className="text-xl text-gray-800 uppercase font-semibold tracking-wide">
-					{venue}
+					{value}
 				</div>
 				<div className="text-lg text-gray-600 tracking-wide">{city}</div>
 				<Divider variant="middle" />
-				<div className="text-sm text-gray-600 mt-2">
+				<div className="text-sm text-gray-600 my-2">
 					Seating Capacity: {capacity}
 				</div>
-				<div
-					style={{
-						width: `${Math.floor(ratings * 2)}ch`,
-					}}
-					className="mt-2 flex overflow-hidden"
-				>
-					<Star />
-					<Star />
-					<Star />
-					<Star />
-					<Star />
+				<div>
+					<Rating name="User Ratings" value={ratings} readOnly precision={0.5} size="small"/>
 				</div>
 			</div>
 		</div>
--- components/Wedding/Venue.jsx
+++ components/Wedding/Venue.jsx
@@ -1,28 +1,26 @@
-import TextField from '@mui/material/TextField';
-import ItemList from './ItemList';
-import { useState } from 'react';
+import Question from '../Misc/Question/Question';
 
 let venues = [
 	{
-		venue: 'Mangalam',
+		value: 'Mangalam',
 		city: 'Ranchi',
-		ratings: 4.8,
+		ratings: 4.4,
 		capacity: 1000,
 	},
 	{
-		venue: 'Shiaoa',
+		value: 'Shiaoa',
 		city: 'Mumbai',
-		ratings: 4,
+		ratings: 4.7,
 		capacity: 1000,
 	},
 	{
-		venue: 'Destination',
+		value: 'Destination',
 		city: 'Ranchi',
 		ratings: 3,
 		capacity: 190,
 	},
 	{
-		venue: 'Mangalam',
+		value: 'Mangalam',
 		city: 'Ranchi',
 		ratings: 5,
 		capacity: 900,
@@ -30,21 +28,7 @@ let venues = [
 ];
 
 const Venue = () => {
-	const [venue, setVenue] = useState('');
-
-	const handleVenueChange = (e) => {
-		setVenue(e.target.value.toLowerCase());
-	};
-
-	return (
-		<div className="flex flex-col gap-10 px-10 w-full sm:w-[36rem]">
-			<div className="text-3xl relative montserrat font-semibold text-center text-gradient capitalize md:text-4xl">
-				wedding venue
-			</div>
-			<TextField label="Venue" variant="filled" onChange={handleVenueChange} />
-			<ItemList list={venues} venue={venue}/>
-		</div>
-	);
+	return <Question list={venues} heading="Wedding venue" label="Venue"/>;
 };
 
 export default Venue;
