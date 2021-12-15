3c3daffc44240937b34b4ead87d1902c569a709d
Shivam <shivam66637@gmail.com>
2021-12-15 23:56:25 +0530

    production

--- pages/wedding/index.jsx
+++ pages/wedding/index.jsx
@@ -9,18 +9,23 @@ export default ({ venues }) => {
 };
 
 export async function getStaticProps(context) {
-	let data = await fetch('http://localhost:3000/api/wedding/venues')
-	let venues = await data.json()
-	
+	const url =
+		process.env.NODE_ENV === 'development'
+			? 'http://localhost:3000/api/wedding/venues'
+			: 'https://hevently.vercel.app/api/wedding/venues';
+
+	let data = await fetch(url);
+	let venues = await data.json();
+
 	if (!venues) {
 		return {
 			notFound: true,
-		}
+		};
 	}
 
 	return {
 		props: {
-			venues: venues.data
+			venues: venues.data,
 		},
 	};
 }
