c18061e521f23318008cf69e92b4bf8be2cb6418
saranggupta11 <sarang.gupta0811@gmail.com>
2021-12-18 19:03:43 +0530

    ui fix

--- components/Login/Login.jsx
+++ components/Login/Login.jsx
@@ -18,8 +18,8 @@ export default function Modal({ showLogin, setShowLogin }) {
     <>
       <div
         className={` ${
-          showLogin ? "" : " pointer-events-none opacity-0 "
-        } justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none transition-all duration-500 px-4`}
+          showLogin ? " opacity-100 " : " pointer-events-none"
+        } justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none transition-all duration-500 px-4 opacity-0`}
       >
         <div className="relative o myw-aut-6 mx-auto max-w-3x bg-bgray-50 bg-gra rounded-2xl">
           <div className="border-0 rounded-2xl shadow-md relative flex flex-col w-full bg-white outline-none focus:outline-none">
--- components/Modal/Modal.jsx
+++ components/Modal/Modal.jsx
@@ -5,8 +5,8 @@ export default function Modal({ showModal, setShowModal, title }) {
     <>
       <div
         className={` ${
-          showModal ? "" : " pointer-events-none opacity-0 "
-        } justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none transition-all duration-500 px-5`}
+          showModal ? " opacity-100 " : " pointer-events-none "
+        } justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none transition-all duration-500 px-5 opacity-0 `}
       >
         <div className="relative o myw-aut-6 mx-auto max-w-3xl bg-bgray-50 rounded-lg">
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
