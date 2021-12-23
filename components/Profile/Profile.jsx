5d4257534b218483ba64a41a36c5fd0787c9f6c4
saranggupta11 <sarang.gupta0811@gmail.com>
2021-12-23 21:31:16 +0530

    confirmation popup

--- components/Profile/Profile.jsx
+++ components/Profile/Profile.jsx
@@ -1,14 +1,28 @@
-import { useContext } from 'react';
-import { UserContext } from '../../context/Users';
+import { useContext, useState } from "react";
+import { UserContext } from "../../context/Users";
+import Confirmation from "../Confirmation/Confirmation";
 
 const Profile = () => {
-	const user = useContext(UserContext)
-	const {email, uid, displayName} = user
-	return (
-		<div className="h-screen w-screen flex items-center justify-center text-4xl">
-			
-		</div>
-	);
+  const user = useContext(UserContext);
+  const { email, uid, displayName } = user;
+  const [showConfirm, setShowConfirm] = useState(false);
+  const handleConfirmation = () => {
+    setShowConfirm((prev) => !prev);
+  };
+  return (
+    <>
+      <div className="mt-20 ml-20">
+        <button
+          onClick={handleConfirmation}
+          type="button"
+          className="border bg-gray-100 text-2xl px-2 py-2"
+        >
+          hello
+        </button>
+      </div>
+      <Confirmation showConfirm={showConfirm} setShowConfirm={setShowConfirm} />
+    </>
+  );
 };
 
 export default Profile;
