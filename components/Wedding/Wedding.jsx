d2fb77e7df3f7b3d96265bd4a25330926b17cc3c
Shivam <shivam66637@gmail.com>
2021-12-23 20:13:33 +0530

    refactor

--- components/Wedding/Wedding.jsx
+++ components/Wedding/Wedding.jsx
@@ -5,61 +5,11 @@ import Venue from './Venue';
 import Schedule from './Schedule';
 import FormFooter from '../Misc/FormFooter';
 import Alert from '../Misc/Alert';
+import Guests from '../Misc/Guests';
 
-import venue from '../../public/form/venue.jpg';
-import catering from '../../public/form/catering.jpg';
-import calender from '../../public/form/calender.jpg';
+import formCover from '../../public/form/form_cover.png';
 
 import { WeddingContext } from '../../context/Wedding';
-
-const QImage = ({ src }) => {
-	return (
-		<div className="relative h-full w-full hidden md:block">
-			<Image
-				src={src}
-				layout="fill"
-				objectFit="cover"
-				priority={true}
-				placeholder="blur"
-			/>
-		</div>
-	);
-};
-
-const Images = ({ position }) => {
-	return (
-		<>
-			<div
-				className={`${
-					position == 0 ? 'opacity-100' : 'opacity-0'
-				} h-full w-full transition-all duration-1000 absolute inset-0`}
-			>
-				<QImage src={venue} />
-			</div>
-			<div
-				className={`${
-					position == 1 ? 'opacity-100' : 'opacity-0'
-				} h-full w-full transition-all duration-1000 absolute inset-0`}
-			>
-				<QImage src={calender} />
-			</div>
-			<div
-				className={`${
-					position == 2 ? 'opacity-100' : 'opacity-0'
-				} h-full w-full transition-all duration-1000 absolute inset-0`}
-			>
-				<QImage src={catering} />
-			</div>
-			<div
-				className={`${
-					position == 3 ? 'opacity-100' : 'opacity-0'
-				} h-full w-full transition-all duration-1000 absolute inset-0`}
-			>
-				<QImage src={venue} />
-			</div>
-		</>
-	);
-};
 
 const Wedding = ({ venues }) => {
 	const [open, setOpen] = useState(false);
@@ -98,16 +48,14 @@ const Wedding = ({ venues }) => {
 				/>
 			</div>
 
-			<div className="fixed w-full h-screen right-0"></div>
-
-			<div className="fixed w-2/5 hidden md:block left-0 h-screen z-10">
-				<Images position={position} />
+			<div className="fixed opacity-30 w-screen h-screen right-0">
+				<Image src={formCover} objectFit="cover" />
 			</div>
 
-			<div className="absolute w-full md:w-3/5 right-0 py-32 flex items-center justify-center">
+			<div className="absolute w-full right-0 py-32 flex items-center justify-center">
 				{position === 0 ? <Venue venues={venues} /> : null}
 				{position === 1 ? <Schedule /> : null}
-				{position === 2 ? <Venue venues={venues} /> : null}
+				{position === 2 ? <Guests /> : null}
 				{position === 3 ? <Venue venues={venues} /> : null}
 
 				<FormFooter
