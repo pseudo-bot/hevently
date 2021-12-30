9e855a611388f719c9a8356ee285e3f575c936c5
saranggupta11 <sarang.gupta0811@gmail.com>
2021-12-30 15:45:47 +0530

    ui fix footer linking

--- components/Misc/EventName.jsx
+++ components/Misc/EventName.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import 'animate.css';
 
 const EventName = ({ showModal, setShowModal, title }) => {
   const [value, setValue] = React.useState("");
@@ -16,7 +17,7 @@ const EventName = ({ showModal, setShowModal, title }) => {
       <div
         className={` ${
           showModal ? " flex " : "hidden"
-        } justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none  px-5 `}
+        } justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none  px-5 animate__animated animate__fadeInDown`}
       >
         <div className="relative o myw-aut-6 mx-auto max-w-3xl bg-bgray-50 rounded-lg">
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
