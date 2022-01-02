92c2945f9e019c29b5874c96187c554f3447bbf4
Shivam <shivam66637@gmail.com>
2022-01-02 18:08:52 +0530

    font fix

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,22 +7,6 @@ class MyDocument extends Document {
 			<Html>
 				<Head>
 				<link rel="icon" type="image/png" href="/icons/logo.png" />
-					<link
-						href="https://fonts.googleapis.com/css?family=Dancing+Script:regular,500,600,700"
-						rel="stylesheet"
-					/>
-					<link
-						href="https://fonts.googleapis.com/css?family=Montserrat+Alternates:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
-						rel="stylesheet"
-					/>
-					<link
-						href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
-						rel="stylesheet"
-					/>
-					<link
-						href="https://fonts.googleapis.com/css?family=Kalam:300,regular,700"
-						rel="stylesheet"
-					/>
 				</Head>
 				<body>
 					<Main />
